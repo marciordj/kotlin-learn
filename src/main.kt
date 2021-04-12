e65c927c7a4a3b5eadf5877893177270b092b071
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-04-12 01:58:06 -0300

    create and instance class

--- src/main.kt
+++ src/main.kt
@@ -1,2 +1,21 @@
 fun main() {
+  println("Welcome to bytebank")
+  val countMarcio = BankCount()
+  val countOther = BankCount()
+
+  countMarcio.name = "Marcio"
+  countMarcio.balance = 104.56
+  countOther.name = "Other"
+  countOther.balance = 109.92
+
+  println("Welcome ${countMarcio.name}, your balance is ${countMarcio.balance}")
+  println("Welcome ${countOther.name}, your balance is ${countOther.balance}")
+}
+
+class BankCount {
+  var name: String = "";
+  var countNumber: Number = 0;
+  var balance: Double = 0.0;
+
+
 }
